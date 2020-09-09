f7e5235c8b2f4b0349820bfac8d222e3043280c8
Tanveer Jan <tjan90@yahoo.com>
2020-09-09 21:42:37 +0200

    key

--- AmazonScraper/settings.py
+++ AmazonScraper/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zqdbu923*jjolflct=60(^fnarzg_i22*o8*c-b3s0&26!9r%&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# django-scraper
+# Amazon Scarper in Django
+
+This is only for educational purposes, use at your own risk
+
+### Installation
+`pip install -r requirements.txt `
+
+### Usage
+`python manage.py runserver`
+
